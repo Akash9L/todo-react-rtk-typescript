99ee6ad2920a38212bd5849192f712e53a4f5821
Akash Navlakha <akash.navlakha@getfierce.com>
2023-05-13 19:26:51 +0530

    Add Search Functionality

--- src/features/TodoList/TodoList.slice.ts
+++ src/features/TodoList/TodoList.slice.ts
@@ -1,23 +1,25 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
 import { getTodos } from 'API'
-import { Task } from 'type.dto'
+import { Task, TaskStatusEnum } from 'type.dto'
 
+
+type Filters = {
+    status: string[];
+    searchString: string;
+}
 type InitialState = {
     loading: boolean;
     todoList: Task[];
-    filteredSet: {
-        filter: string;
-        searchString: string;
-        filteredTodos: Task[];
-    }
+    filteredTodos: Task[];
+    filters: Filters;
     error: string;
 }
 const initialState: InitialState = {
     loading: false,
     todoList: [],
-    filteredSet: {
-        filter: '',
-        filteredTodos: [],
+    filteredTodos: [],
+    filters: {
+        status: [TaskStatusEnum.COMPLETED, TaskStatusEnum.TODO],
         searchString: '',
     },
     error: ''
@@ -27,11 +29,25 @@ const initialState: InitialState = {
 export const fetchTodoList = createAsyncThunk('todo/fetchTodoList', () => {
     return getTodos()
 })
-
+function getFilteredTodos(todoList: Task[], filters: Filters) {
+    return todoList.filter(item => {
+        return filters.status.includes(item.status)
+            && item.name.toLowerCase().includes(filters.searchString.toLowerCase())
+    });
+}
 const todoSlice = createSlice({
     name: 'todo',
     initialState,
-    reducers: {},
+    reducers: {
+        updateFilters(state, action: PayloadAction<Filters>) {
+            //Update Filtered Todo whenever a filter is updated
+            if (action.payload.status) {
+                state.filters.status = action.payload.status;
+            }
+            state.filters.searchString = action.payload.searchString || "";
+            state.filteredTodos = getFilteredTodos(state.todoList, state.filters);
+        }
+    },
     extraReducers: builder => {
         builder.addCase(fetchTodoList.pending, state => {
             state.loading = true
@@ -42,6 +58,7 @@ const todoSlice = createSlice({
                 state.loading = false
                 state.todoList = action.payload
                 state.error = ''
+                state.filteredTodos = getFilteredTodos(state.todoList, state.filters);
             }
         )
         builder.addCase(fetchTodoList.rejected, (state, action) => {
@@ -53,3 +70,4 @@ const todoSlice = createSlice({
 })
 
 export default todoSlice.reducer
+export const { updateFilters } = todoSlice.actions;
--- src/features/TodoList/TodoList.view.tsx
+++ src/features/TodoList/TodoList.view.tsx
@@ -4,8 +4,9 @@ import { fetchTodoList } from "./TodoList.slice";
 import { TaskView } from "features/Task/Task.view";
 import { AddTaskView } from "features/AddTask/AddTask.view";
 import "./Todo.scss";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { AiOutlinePlus } from "react-icons/ai";
+import { updateFilters } from "./TodoList.slice";
 
 export function TodoList() {
   //Open show Add Task Modal
@@ -20,9 +21,30 @@ export function TodoList() {
 
   const todoSlice = useAppSelector((state) => state.todo);
 
+  function handleSearchStringChange(event: React.ChangeEvent<any>): void {
+    dispatch(
+      updateFilters({
+        searchString: event.target.value || "",
+        status: todoSlice.filters.status,
+      })
+    );
+  }
+
   return (
     <div>
       <h1 className="mt-5">List of Tasks</h1>
+
+      <div>
+        <Form.Control
+          type="search"
+          placeholder="Search by name"
+          className="me-2"
+          aria-label="Search"
+          value={todoSlice.filters.searchString}
+          onChange={handleSearchStringChange}
+        />
+        {todoSlice.filters.searchString}
+      </div>
 
       {/* Show Task */}
       {todoSlice.loading && <div>Loading...</div>}
@@ -42,8 +64,8 @@ export function TodoList() {
             </Button>
           </div>
         </div>
-        {!todoSlice.loading && todoSlice.todoList.length
-          ? todoSlice.todoList.map((task) =>
+        {!todoSlice.loading && todoSlice.filteredTodos.length
+          ? todoSlice.filteredTodos.map((task) =>
               TaskView({
                 task,
               })
