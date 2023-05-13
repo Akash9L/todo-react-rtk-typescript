562658260e6e6a8c7409c24784d5481c127b6b92
Akash Navlakha <akash.navlakha@getfierce.com>
2023-05-13 17:53:08 +0530

    Replace favicon and change Site Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css';
-import { TodoList } from 'features/TodoList/TodoList.view';
+import "./App.scss";
+import { TodoList } from "features/TodoList/TodoList.view";
 
 function App() {
   return (
