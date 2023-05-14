def0a2ecebc4b01510744990c580c213430957a3
Akash Navlakha <akash.navlakha@getfierce.com>
2023-05-14 14:29:27 +0530

    Add Filters to the view

--- src/features/TodoList/TodoList.view.tsx
+++ src/features/TodoList/TodoList.view.tsx
@@ -7,6 +7,7 @@ import "./Todo.scss";
 import { Button, Form } from "react-bootstrap";
 import { AiOutlinePlus } from "react-icons/ai";
 import { updateFilters } from "./TodoList.slice";
+import { TaskStatusEnum } from "type.dto";
 
 export function TodoList() {
   //Open show Add Task Modal
@@ -30,20 +31,77 @@ export function TodoList() {
     );
   }
 
+  function onStatusFilterChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    const status = event.target.value;
+    const isChecked = event.target.checked;
+    console.log(
+      "onStatusFilterChange",
+      isChecked,
+      status,
+      todoSlice.filters.status
+    );
+    if (isChecked) {
+      //Add element to status filter
+      dispatch(
+        updateFilters({
+          searchString: todoSlice.filters.searchString,
+          status: todoSlice.filters.status
+            .filter((item) => item !== status)
+            .concat(status),
+        })
+      );
+    } else {
+      //Remove element from status filter
+      dispatch(
+        updateFilters({
+          searchString: todoSlice.filters.searchString,
+          status: todoSlice.filters.status.filter((item) => item !== status),
+        })
+      );
+    }
+  }
+
   return (
     <div>
       <h1 className="mt-5">List of Tasks</h1>
-
-      <div>
-        <Form.Control
-          type="search"
-          placeholder="Search by name"
-          className="me-2"
-          aria-label="Search"
-          value={todoSlice.filters.searchString}
-          onChange={handleSearchStringChange}
-        />
-        {todoSlice.filters.searchString}
+      <div className="row px-5">
+        <div className="col-md-8 py-2">
+          <Form.Control
+            type="search"
+            placeholder="Search by Task Name"
+            className="me-2"
+            aria-label="Search"
+            value={todoSlice.filters.searchString}
+            onChange={handleSearchStringChange}
+          />
+          {todoSlice.filters.searchString}
+        </div>
+        <div className="col-md-4 py-2">
+          <Form.Check
+            inline
+            type="switch"
+            id="pending-switch"
+            label="PENDING"
+            value={TaskStatusEnum.TODO}
+            onChange={onStatusFilterChange}
+            checked={todoSlice.filters.status.some(
+              (item) => item === TaskStatusEnum.TODO
+            )}
+          />
+          <Form.Check
+            inline
+            type="switch"
+            id="completed-switch"
+            label="COMPLETED"
+            value={TaskStatusEnum.COMPLETED}
+            onChange={onStatusFilterChange}
+            checked={todoSlice.filters.status.some(
+              (item) => item === TaskStatusEnum.COMPLETED
+            )}
+          />
+        </div>
       </div>
 
       {/* Show Task */}
